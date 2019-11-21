2882940265883f287932c8a5f042a7c4c75c8b21
sachin <sbwakle@hotmail.com>
2019-11-21 10:12:33 +0530

    exercise 13 of lambda function tutorial from javabrains

---
+++ lambda-app/src/main/java/io/sachin/lambda/functions/DisplayAll.java
@@ -0,0 +1,10 @@
+package io.sachin.lambda.functions;
+
+import java.util.List;
+import io.sachin.lambda.model.Person;
+
+@FunctionalInterface
+public interface DisplayAll {
+
+	public void display(List<Person> person );
+}
